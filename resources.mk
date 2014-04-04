34023fa43584a761956eff533f459b26abb04923
Bangprovn <bangprovn@gmail.com>
2014-04-04 12:24:12 +0700

    Fix offline charging

--- resources.mk
+++ resources.mk
@@ -1,19 +1,19 @@
 # Offline charging animation
 PRODUCT_COPY_FILES += \
-     device/pantech/common/animations/charging_animation_01.png:system/semc/chargemon/data/charging_animation_01.png \
-     device/pantech/common/animations/charging_animation_02.png:system/semc/chargemon/data/charging_animation_02.png \
-     device/pantech/common/animations/charging_animation_03.png:system/semc/chargemon/data/charging_animation_03.png \
-     device/pantech/common/animations/charging_animation_04.png:system/semc/chargemon/data/charging_animation_04.png \
-     device/pantech/common/animations/charging_animation_05.png:system/semc/chargemon/data/charging_animation_05.png \
-     device/pantech/common/animations/charging_animation_06.png:system/semc/chargemon/data/charging_animation_06.png \
-     device/pantech/common/animations/charging_animation_07.png:system/semc/chargemon/data/charging_animation_07.png \
-     device/pantech/common/animations/non-charging_animation_01.png:system/semc/chargemon/data/non-charging_animation_01.png \
-     device/pantech/common/animations/non-charging_animation_02.png:system/semc/chargemon/data/non-charging_animation_02.png \
-     device/pantech/common/animations/non-charging_animation_03.png:system/semc/chargemon/data/non-charging_animation_03.png \
-     device/pantech/common/animations/non-charging_animation_04.png:system/semc/chargemon/data/non-charging_animation_04.png \
-     device/pantech/common/animations/non-charging_animation_05.png:system/semc/chargemon/data/non-charging_animation_05.png \
-     device/pantech/common/animations/non-charging_animation_06.png:system/semc/chargemon/data/non-charging_animation_06.png \
-     device/pantech/common/animations/non-charging_animation_07.png:system/semc/chargemon/data/non-charging_animation_07.png
+     device/pantech/common/animations/charging_animation_01.png:system/sky/chargemon/data/charging_animation_01.png \
+     device/pantech/common/animations/charging_animation_02.png:system/sky/chargemon/data/charging_animation_02.png \
+     device/pantech/common/animations/charging_animation_03.png:system/sky/chargemon/data/charging_animation_03.png \
+     device/pantech/common/animations/charging_animation_04.png:system/sky/chargemon/data/charging_animation_04.png \
+     device/pantech/common/animations/charging_animation_05.png:system/sky/chargemon/data/charging_animation_05.png \
+     device/pantech/common/animations/charging_animation_06.png:system/sky/chargemon/data/charging_animation_06.png \
+     device/pantech/common/animations/charging_animation_07.png:system/sky/chargemon/data/charging_animation_07.png \
+     device/pantech/common/animations/non-charging_animation_01.png:system/sky/chargemon/data/non-charging_animation_01.png \
+     device/pantech/common/animations/non-charging_animation_02.png:system/sky/chargemon/data/non-charging_animation_02.png \
+     device/pantech/common/animations/non-charging_animation_03.png:system/sky/chargemon/data/non-charging_animation_03.png \
+     device/pantech/common/animations/non-charging_animation_04.png:system/sky/chargemon/data/non-charging_animation_04.png \
+     device/pantech/common/animations/non-charging_animation_05.png:system/sky/chargemon/data/non-charging_animation_05.png \
+     device/pantech/common/animations/non-charging_animation_06.png:system/sky/chargemon/data/non-charging_animation_06.png \
+     device/pantech/common/animations/non-charging_animation_07.png:system/sky/chargemon/data/non-charging_animation_07.png
 
 # Boot Logo
 ifneq ($(FXP_BUILD),true)
